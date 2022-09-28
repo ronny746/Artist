c4cd1fb793002ff0003f5c745b8bd9721d64141a
Rohit Rana <rkrana6631@gmail.com>
2022-09-29 00:45:14 +0530

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <h2>Meri Kaddu</h2>
     <img src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691__340.png" alt="Girl in a jacket" width="500"
         height="600">
+    <img src="https://i.pinimg.com/originals/18/aa/2a/18aa2a1bf496fee184b0293994e5e589.png" alt="Girl in a jacket"
+        width="500" height="600">
 
 </body>
 
